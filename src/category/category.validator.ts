f0cce4b33802588f6b7f15b156ff475111189ae0
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2023-09-19 18:26:51 +0000

    feat(category): add category validator

---
+++ src/category/category.validator.ts
@@ -0,0 +1,34 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+import { ClassValidatorFields } from '../shared/domain/validators/class-validator-fields';
+import { Category } from './category.entity';
+
+class CategoryRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description: string | null;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  is_active: boolean;
+
+  constructor({ name, description, is_active }: Category) {
+    Object.assign(this, { name, description, is_active })
+  }
+}
+
+export class CategoryValidator extends ClassValidatorFields<CategoryRules> {
+  public validate(entity: Category) {
+    return super.validate(new CategoryRules(entity));
+  }
+}
+
+export class CategoryValidatorFactory {
+  public static create(): CategoryValidator {
+    return new CategoryValidator();
+  }
+}
